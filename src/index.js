69e17e3968aa5af4519385bb9464a751377a0fac
Johannes Naylor <jonaylor89@gmail.com>
2023-08-10 13:59:30 +0200

    analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import { inject } from '@vercel/analytics';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+ 
+inject();
 
 ReactDOM.render( < React.StrictMode >
     <App />
