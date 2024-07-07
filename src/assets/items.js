c057d915bfc4f6f8f7be49ecc664405fea2a1d67
Johannes Naylor <jonaylor89@gmail.com>
2024-07-06 20:07:41 -0400

    boom

--- src/assets/items.js
+++ src/assets/items.js
@@ -1,65 +1,64 @@
-import website from '../assets/website.png'
+import website from "../assets/website.png";
 // import instagram from '../assets/instagram.png'
-import appstore from '../assets/playstore.png'
+import appstore from "../assets/playstore.png";
 // import youtube from '../assets/youtube.jpeg'
-import dribbble from '../assets/dribbble.png'
-import github from '../assets/github.png'
-// import telegram from '../assets/telegram.png'
-import twitter from '../assets/twitter.png'
-import blog from '../assets/blog.png'
-import intheloop from '../assets/intheloop.png'
-import tapped from "../assets/tapped.png"
+import dribbble from "../assets/dribbble.png";
+import github from "../assets/github.png";
+import telegram from "../assets/telegram.png";
+import twitter from "../assets/twitter.png";
+import blog from "../assets/blog.png";
+import intheloop from "../assets/intheloop.png";
+import tapped from "../assets/tapped.png";
 
 const items = [
-    {
-        "title": "Website",
-        "subtitle": "Look at my work!",
-        "image": website,
-        "link": "https://jonaylor.xyz" //your personal website or portfolio  link
-    },
-    {
-        "title": "Blog",
-        "subtitle": "The latest content for what I've written",
-        "image": blog,
-        "link": "https://blog.jonaylor.xyz"// Blog link
-    },
-    {
-        "title": "GitHub",
-        "subtitle": "@jonaylor89 | 🏠 of my open-source projects",
-        "image": github,
-        "link": "https://github.com/jonaylor89" //Github Profile link
-    },
-    {
-        "title": "Twitter",
-        "subtitle": "@jonaylor89 | Don't forget to follow me 😉",
-        "image": twitter,
-        "link": "https://twitter.com/jonaylor89"// twitter profile link 
-    },
-    {
-      "title": "Tapped",
-      "subtitle": "A live music portfolio for musicians",
-      "image": tapped,
-      "link": "https://tapped.ai"
-    },
-    {
-        "title": "In The Loop",
-        "subtitle": "The platform tailored for artists and producers to collaborate",
-        "image": intheloop,
-        "link": "https://intheloopstudio.com" // In The Loop landing page
-    },
-    {
-        "title": "Apps",
-        "subtitle": "Hub of my awesome 🔥 Apps",
-        "image": appstore,
-        "link": "https://play.google.com/store/apps/developer?id=John+Naylor"// google play linik
-    },
-    {
-        "title": "Dribbble",
-        "subtitle": "@jonaylor89 | Hub to the Shots of my 😎 apps ",
-        "image": dribbble,
-        "link": "https://dribbble.com/jonaylor89" // Dribbble profile link 
-    },
+  {
+    title: "Website",
+    subtitle: "Look at my work!",
+    image: website,
+    link: "https://jonaylor.xyz", //your personal website or portfolio  link
+  },
+  {
+    title: "Blog",
+    subtitle: "The latest content for what I've written",
+    image: blog,
+    link: "https://blog.jonaylor.xyz", // Blog link
+  },
+  {
+    title: "GitHub",
+    subtitle: "@jonaylor89 | 🏠 of my open-source projects",
+    image: github,
+    link: "https://github.com/jonaylor89", //Github Profile link
+  },
+  {
+    title: "Twitter",
+    subtitle: "@jonaylor89 | Don't forget to follow me 😉",
+    image: twitter,
+    link: "https://twitter.com/jonaylor89", // twitter profile link
+  },
+  {
+    title: "Tapped Ai",
+    subtitle: "the leading live music database",
+    image: tapped,
+    link: "https://tapped.ai",
+  },
+  {
+    title: "In The Loop",
+    subtitle: "The platform tailored for artists and producers to collaborate",
+    image: intheloop,
+    link: "https://intheloopstudio.com", // In The Loop landing page
+  },
+  {
+    title: "Apps",
+    subtitle: "Hub of my awesome 🔥 Apps",
+    image: appstore,
+    link: "https://play.google.com/store/apps/developer?id=John+Naylor", // google play linik
+  },
+  {
+    title: "Telegram",
+    subtitle: "@jonaylor89 | Let's chat!",
+    image: telegram,
+    link: "https://telegram.me/jonaylor89", // telegram profile link
+  },
+];
 
-]
-
-export default items
+export default items;
